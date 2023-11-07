04cd09f1cb28cbed0f872e0587343bca4db410bc
Rai Joia <megac154@gmail.com>
2023-11-07 07:04:27 -0300

    bookwiz(livroDAO): metodo adicionar livro implementado

--- src/main/java/com/bookwiz/bookwiz/LivroDAO.java
+++ src/main/java/com/bookwiz/bookwiz/LivroDAO.java
@@ -13,21 +13,17 @@ import java.sql.SQLException;
 public class LivroDAO {
     public void adicionarLivro(Livro livro) throws SQLException, Exception {
         String sql = "INSERT INTO TB_LIVRO (LV_TITULO, LV_AUTOR, LV_DESC, LV_GEN, LV_DTCAD) VALUES (?, ?, ?, ?, NOW());";
-        String sql2 = "INSERT INTO TB_AVA (AVA_DESC) VALUES (?)";
         
         try (
             var factory = ConnectionFactory.conectar();
             var ps = factory.prepareStatement(sql);
-            var ps2 = factory.prepareStatement(sql2);
         ){
             ps.setString(1, livro.getTitulo());
             ps.setString(2, livro.getAutor());
             ps.setString(3, livro.getDesc());
             ps.setInt(4, livro.getGenero());
-            ps2.setInt(1, livro.getNota());
             
             ps.execute();
-            ps2.execute();
         }
     }
 }
