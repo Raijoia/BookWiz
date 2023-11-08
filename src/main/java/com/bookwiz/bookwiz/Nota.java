c5ec8cb332e5b53aa5b9986d11c5c141683d276b
Rai Joia <megac154@gmail.com>
2023-11-08 11:04:28 -0300

    bookwiz(nota): classe nota criada com atributo e getters e setters

---
+++ src/main/java/com/bookwiz/bookwiz/Nota.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bookwiz.bookwiz;
+
+/**
+ *
+ * @author rai
+ */
+public class Nota {
+    private int nota;
+    
+    public Nota(int nota){
+        this.nota = nota;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+}
